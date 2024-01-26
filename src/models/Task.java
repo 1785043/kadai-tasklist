a917a0530298bb12fd278c11f868fbdb672e6816
shuto.chida <vegalta1783@gmail.com>
2024-01-26 17:40:15 +0900

    削除

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,26 +7,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@NamedQueries({
-    @NamedQuery(
-        name = "getAllTasks",
-        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
-    )
-})
 @Table(name = "tasks")
 public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(name = "title", length = 255, nullable = false)
-    private String title;
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
@@ -43,14 +32,6 @@ public class Task {
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getContent() {
@@ -77,3 +58,4 @@ public class Task {
         this.updated_at = updated_at;
     }
 }
+
