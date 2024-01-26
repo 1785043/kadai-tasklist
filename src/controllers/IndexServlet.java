445c0e68e7f8801583ddf368f5db9c128b518a55
shuto.chida <vegalta1783@gmail.com>
2024-01-26 16:49:57 +0900

    getAllMessagesを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Message;
+import models.Task;
 import utils.DBUtil;
 
 /**
@@ -32,17 +31,13 @@ public class IndexServlet extends HttpServlet {
         /**
          * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
          */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            EntityManager em = DBUtil.createEntityManager();
 
-        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+            List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
+            response.getWriter().append(Integer.valueOf(tasks.size()).toString());
 
-        em.close();
-
-        request.setAttribute("messages", messages);
-
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
-        rd.forward(request, response);
-    }
+            em.close();
+        }
 
 }
